75b2ade88e6b0cb249038f07c9759ae5e54f61f7
Ganesh Kumar <your.email@example.com>
2025-06-30 18:30:16 +0530

    Initial commit

--- app/src/main/java/com/example/statehoistingapp/CounterScreen.kt
+++ app/src/main/java/com/example/statehoistingapp/CounterScreen.kt
@@ -41,7 +41,7 @@ fun CounterScreen() {
             onIncrement = { count++ }
         )
 
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(26.dp))
 
         Button(onClick = {
             name = ""
